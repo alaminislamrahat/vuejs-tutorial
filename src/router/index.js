fa7a4d04053d3cd37730cd67b4fbae879b40fe75
alaminislamrahat <alaminislamrahat@gmail.com>
2025-08-17 19:05:15 +0600

    jobs pag added to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeVue from "@/views/HomeVue.vue";
+import JobsView from "@/views/JobsView.vue";
 
 const router = createRouter({
     history : createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,12 @@ const router = createRouter({
             path : '/',
             name : 'Home',
             component : HomeVue
-        }
+        },
+        {
+            path : '/jobs',
+            name : 'Jobs',
+            component : JobsView
+        },
     ]
 })
 
