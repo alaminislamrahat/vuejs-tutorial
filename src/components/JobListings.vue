11d8c4ae3c026ddb2a412e583bf35f6a7ad11d20
alaminislamrahat <alaminislamrahat@gmail.com>
2025-08-17 19:14:55 +0600

    router link added

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import jobData from '@/jobs.json';
 import { ref, defineProps} from 'vue';
 import JoblistingSingle from './JoblistingSingle.vue';
+import { RouterLink } from 'vue-router';
 
 defineProps({
     limit : Number,
@@ -29,10 +30,10 @@ console.log('first', jobs.value)
 
     </section>
      <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
    
